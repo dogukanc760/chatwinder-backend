ce9e61691e561f7e0b7ac3977b07126739a9ee07
CoderBig <49120922+dogukanc760@users.noreply.github.com>
2021-12-12 01:31:07 +0300

    yeniler 12.12
    Yeni güncelleme bio eklendi

--- models/User.js
+++ models/User.js
@@ -5,9 +5,10 @@ const UserSchema = new mongoose.Schema(
     {
         username:{type:String, required:true, unique:true},
         email:{type:String, required:true, unique:true},
-        name:{type:String, required:true, unique:true},
-        surname:{type:String, required:true, unique:true},
+        name:{type:String, required:true},
+        surname:{type:String, required:true},
         password:{type:String, required:true},
+        bioDesc:{type:String, required:true},
         searchType:{type:String, default:null},
         country:{type:String, default:null},
         city:{type:String, default:null},
